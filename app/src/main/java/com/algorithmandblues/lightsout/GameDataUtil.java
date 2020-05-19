e0adcd069a23a78690126579cc4b9b70816bb989
RohitBagda <rbagda@macalester.edu>
2020-05-19 01:41:48 -0500

    Add GameDataUtil

---
+++ app/src/main/java/com/algorithmandblues/lightsout/GameDataUtil.java
@@ -0,0 +1,53 @@
+package com.algorithmandblues.lightsout;
+
+import java.util.Stack;
+
+public class GameDataUtil {
+
+    public static byte[] stringToByteArray(String str) {
+        String[] split = str.split(",");
+        byte[] numbers = new byte[split.length];
+        int i=0;
+        for(String s : split) {
+            numbers[i++] = Byte.parseByte(s);
+        }
+        return numbers;
+    }
+
+    public static String byteArrayToString(byte[] array) {
+
+        StringBuilder str = new StringBuilder(array.length);
+        for (int i = 0; i < array.length; i++) {
+            str.append((int) array[i]);
+            if(i!= array.length-1) {
+                str.append(",");
+            }
+        }
+        return str.toString();
+    }
+
+    public static Stack<Integer> StringToIntegerStack(String str) {
+        Stack<Integer> stack = new Stack<>();
+
+        if(str == null || str.isEmpty()) {
+            return stack;
+        }
+
+        String[] split = str.split(",");
+        for(String s : split) {
+            stack.push(Integer.parseInt(s));
+        }
+        return stack;
+    }
+
+    public static String IntegerStackToString(Stack<Integer> stack) {
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < stack.size(); i++) {
+            str.append((int) stack.get(i));
+            if(i!= stack.size()-1) {
+                str.append(",");
+            }
+        }
+        return str.toString();
+    }
+}
