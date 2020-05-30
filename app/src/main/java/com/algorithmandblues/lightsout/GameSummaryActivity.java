dc3558c885014c47ea7003d5892a3844677f0c0e
RohitBagda <rbagda@macalester.edu>
2020-05-30 05:57:05 -0500

    Migrated to Android X

--- app/src/main/java/com/algorithmandblues/lightsout/GameSummaryActivity.java
+++ app/src/main/java/com/algorithmandblues/lightsout/GameSummaryActivity.java
@@ -7,7 +7,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
--- app/src/main/java/com/algorithmandblues/lightsout/LevelStatusFragment.java
+++ app/src/main/java/com/algorithmandblues/lightsout/LevelStatusFragment.java
@@ -1,11 +1,8 @@
 package com.algorithmandblues.lightsout;
 
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
